35d6c0750a945ddc702a7c680ccd450f17815da8
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-05-07 13:31:11 +0100

    Completed first test

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
+const artistRouter = require("./routes/artist");
 
 const app = express();
 
-app.use(express.json());
+app.use("/", artistRouter);
 
 app.get("/", (_req, res) => {
   res.send("Hello World!");
---
+++ src/controllers/artist.js
@@ -0,0 +1,7 @@
+const createArtist = (_req, res) => {
+  res.sendStatus(201);
+};
+
+module.exports = {
+  createArtist,
+};
