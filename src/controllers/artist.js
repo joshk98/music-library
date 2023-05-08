dbc2fbcd76bc6ddfafedd842098ce365022407c0
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2023-05-08 16:58:27 +0100

    Updating Artists Completed

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -16,4 +16,92 @@ const createArtist = async (req, res) => {
   }
 };
 
-module.exports = { createArtist };
+const getAllArtists = async (_, res) => {
+  try {
+    const { rows } = await db.query("SELECT * from Artists");
+    res.status(200).json(rows);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+const getArtistById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query("SELECT * from Artists WHERE id = $1", [id]);
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+const putArtist = async (req, res) => {
+  const { name, genre } = req.body;
+  const { id } = req.params;
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(
+      "UPDATE Artists SET name = $1, genre = $2 WHERE id = $3 RETURNING *",
+      [name, genre, id]
+    );
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+const patchArtist = async (req, res) => {
+  const { name, genre } = req.body;
+  const { id } = req.params;
+
+  let query, params;
+
+  if (name && genre) {
+    query =
+      "UPDATE Artists SET name = $1, genre = $2 WHERE id = $3 RETURNING *";
+    params = [name, genre, id];
+  } else {
+    query =
+      "UPDATE Artists SET " +
+      (name ? "name = $1" : "genre = $1") +
+      "WHERE id = $2 RETURNING *";
+    params = [name || genre, id];
+  }
+
+  try {
+    const {
+      rows: [artist],
+    } = await db.query(query, params);
+
+    if (!artist) {
+      return res.status(404).json({ message: `artist ${id} does not exist` });
+    }
+
+    res.status(200).json(artist);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+module.exports = {
+  createArtist,
+  getAllArtists,
+  getArtistById,
+  putArtist,
+  patchArtist,
+};
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -4,5 +4,9 @@ const artistController = require("../controllers/artist");
 const router = express.Router();
 
 router.post("/artists", artistController.createArtist);
+router.get("/artists", artistController.getAllArtists);
+router.get("/artists/:id", artistController.getArtistById);
+router.put("/artists/:id", artistController.putArtist);
+router.patch("/artists/:id", artistController.patchArtist);
 
 module.exports = router;
